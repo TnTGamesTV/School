2c2597609303b94a25cc45e20f0483a850dbd902
TnTGamesTV <finn.tegeler@web.de>
2016-12-04 15:49:55 +0100

    Create MaterialType.java

---
+++ modelierung/MaterialType.java
@@ -0,0 +1,9 @@
+public class MaterialType {
+
+	public final static Material MATERIAL_WOOD = new Material(1,"Wood");
+	public final static Material MATERIAL_STONE = new Material(2,"Stone");
+	public final static Material MATERIAL_GOLD = new Material(3,"Gold");
+	public final static Material MATERIAL_IRON = new Material(4,"Iron");
+	public final static Material MATERIAL_DIAMOND = new Material(5,"Diamond");
+	
+}
