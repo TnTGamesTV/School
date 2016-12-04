6b786d65344dea384730794aceff35f5004df677
TnTGamesTV <finn.tegeler@web.de>
2016-12-04 15:50:54 +0100

    Create MagicType.java

---
+++ modelierung/MagicType.java
@@ -0,0 +1,7 @@
+public class MagicType {
+
+	public final static Magic MAGIC_FIRE = new Magic(3,1);
+	public final static Magic MAGIC_POISON = new Magic(2,2);
+	public final static Magic MAGIC_WATER = new Magic(1,3);
+	
+}
